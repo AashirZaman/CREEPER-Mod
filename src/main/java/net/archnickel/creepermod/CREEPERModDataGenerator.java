05aeb5a22303db04372d7847163a2214f362fdbe
unknown <ashirqwertz123@gmail.com>
2024-02-24 09:59:40 +0600

    Initial commit

---
+++ src/main/java/net/archnickel/creepermod/CREEPERModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.archnickel.creepermod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CREEPERModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
